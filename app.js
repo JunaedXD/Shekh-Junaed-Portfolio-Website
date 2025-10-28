3e18436128c5153057ebce66de7f0b89f4a463af
Shekh Junaed <sheikhjunaed705@gmail.com>
2025-10-28 14:06:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const slider2 = document.querySelector(".projects-slider");
-document.querySelector(".shield-btn.left").addEventListener("click", () => {
-  slider2.scrollBy({ left: -300, behavior: "smooth" });
-});
-document.querySelector(".shield-btn.right").addEventListener("click", () => {
-  slider2.scrollBy({ left: 300, behavior: "smooth" });
-});
+
 
 const paragraph = document.querySelector(".animated-text");
 const words = paragraph.textContent.trim().split(/\s+/);
